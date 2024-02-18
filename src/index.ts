0a585374f60647b55ed6d5cec7f95c777e8e684f
gedion <gedion@domain.com>
2024-02-18 18:08:34 +0300

    up

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,13 @@ import cors from "cors"
 const app = express();
 app.use(cors())
 app.use(bodyParser.json());
-const port = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-const DB_URL = process.env.LOCAL_DB_URL
+const DB_URL = process.env.DB_URL
 
 if (!DB_URL) {
     console.error('Database URL not found in environment variables');
-    process.exit(1); // Exit the process if the database URL is not provided
+    process.exit(1);
 }
   
 mongoose.connect(DB_URL).then(() => {
@@ -24,11 +24,11 @@ mongoose.connect(DB_URL).then(() => {
 });
 import songsRoute from "./routers/songsRoute"
 import staticsRoute from "./routers/statisticsRoute"
-//const songsRoute = require ("./routers/songsRoute")
+
 app.use(songsRoute)
 app.use(staticsRoute)
 
 
-app.listen(3000, () => {
-  console.log(`[server]: Server is running at http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`[Server]: Server is running at http://localhost:${PORT}`);
 });
